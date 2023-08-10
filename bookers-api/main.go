572e0ed88bb19a1ddf68147005ec106a247d2b51
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-08-10 15:34:16 +0900

    add: 雛形作成

--- bookers-api/main.go
+++ bookers-api/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+type book struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var books = []book{
+	{ID: "1", Title: "一番目", Body: "一番目のbook"},
+	{ID: "2", Title: "二番目", Body: "二番目のbook"},
+	{ID: "3", Title: "三番目", Body: "三番目のbook"},
+}
+
+func getBooks(c *gin.Context) {
+	c.JSON(http.StatusOK, books)
+}
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, Bookers!",
-		})
-	})
+	router.GET("/books", getBooks)
 
-	router.Run(":3000")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("サーバーの起動に失敗しました: %v", err)
+	}
 }
